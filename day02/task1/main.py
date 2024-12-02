3a4f63ca1023ea15405acb2733b9aeb10fcf7d7d
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2024-12-02 09:30:10 +0100

    day02

---
+++ day02/task1/main.py
@@ -0,0 +1,49 @@
+"""
+
+Author: GoldenKoopa
+Puzzle: Advent of Code (year=2024 ; day=2 ; task=1)
+https://adventofcode.com/2024/day/2
+"""
+
+import sys
+import pathlib
+sys.path.append(pathlib.Path(__file__).parent.parent.parent.absolute().as_posix())
+
+import functools
+print = functools.partial(print, flush=True)
+
+def is_monotone(lst):
+    if len(lst) < 2:
+        return True
+    
+    if lst[0] < lst[1]:
+        for i in range(len(lst) - 1):
+            if lst[i] >= lst[i + 1]:
+                return False
+        return True
+
+    if lst[0] > lst[1]:
+        for i in range(len(lst) - 1):
+            if lst[i] <= lst[i + 1]:
+                return False
+        return True
+
+def has_valid_distances(lst):
+    for i in range(len(lst) - 1):
+        diff = abs(lst[i] - lst[i + 1])
+        if diff < 1 or diff > 3:
+            return False
+    return True
+
+def main():
+    sum = 0
+    for line in sys.stdin:
+        report = list(map(int, line.split())) 
+        if is_monotone(report) and has_valid_distances(report):
+            sum += 1
+    
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
